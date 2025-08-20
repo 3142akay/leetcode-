83ca428b137d11db266a018e9db881144b032916
Destro <117672208+3142akay@users.noreply.github.com>
2025-08-20 16:53:39 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (71.82%) - LeetSync

---
+++ 198-house-robber/house-robber.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n==1)
+        return nums[0];
+        vector<int> t(n+1);
+        t[0]=0;
+        t[1]=nums[0];
+        for(int i=2; i<=n; i++){
+            int skip = t[i-1];
+            int steal = nums[i-1]+t[i-2];
+            t[i]= max(skip, steal);
+        }
+        return t[n];
+    }
+};
