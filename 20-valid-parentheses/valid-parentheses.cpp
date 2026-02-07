6fd264558940ffbd95817366165ee3f3141f9339
Destro <117672208+3142akay@users.noreply.github.com>
2026-02-08 00:05:16 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (10.96%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -2,23 +2,23 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> st;
-        for (int i =0; i<s.length(); i++){
-            char c = s[i];
-            if(c =='(' || c=='[' || c=='{'){
-                st.push(c);
+        unordered_map<char, char> pairs ={
+            {')', '('},
+            {']', '['},
+            {'}', '{'},
+        };
+        for (char c: s){
+            if (pairs.count(c)){
+                if(st.empty() || st.top()!= pairs[c]){
+                return false;
+
             }
-            else{
-                if (st.empty()){
-                    return false;
-                }
-                char top = st.top();
-                st.pop();
-                if(c==')' && top !='(') return false;
-                if(c==']' && top!='[') return false;
-                if(c=='}' && top!='{') return false;
+            st.pop();
+            } 
+           else {
+            st.push(c);
             }
-        }
-        
+        }   
         return st.empty();
     }
 };
